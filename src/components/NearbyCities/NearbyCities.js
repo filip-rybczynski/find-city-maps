6bd1e46b36009c2055fe949152c480b6b719c657
Filip Rybczyński <filipryba@gmail.com>
2022-06-02 23:49:30 +0200

    JS: nearby city button show/hide city info when clicked

--- src/components/NearbyCities/NearbyCities.js
+++ src/components/NearbyCities/NearbyCities.js
@@ -1,9 +1,10 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import "./nearby-cities.scss";
 
 function NearbyCities({ mainCity, setApiCallsLeft }) {
-
-    const [nearbyCitiesArr, setNearbyCitiesArr] = useState([])
+  const [nearbyCitiesArr, setNearbyCitiesArr] = useState([]);
+  const [selectedCity, setSelectedCity] = useState(null);
+  const [activeButton, setActiveButton] = useState(null);
 
   const fetchOptions = {
     method: "GET",
@@ -17,49 +18,93 @@ function NearbyCities({ mainCity, setApiCallsLeft }) {
     fetch(
       `https://wft-geo-db.p.rapidapi.com/v1/geo/cities/${id}/nearbyCities?limit=10&radius=100&minPopulation=100000&types=CITY&sort=-population`,
       fetchOptions
-    ).then((response) => {
-      const callsRemaining = response.headers.get(
-        "x-ratelimit-requests-remaining"
-      );
-      
-      setApiCallsLeft(callsRemaining);
+    )
+      .then((response) => {
+        const callsRemaining = response.headers.get(
+          "x-ratelimit-requests-remaining"
+        );
 
-      return response.json()
-    })
-    .then((response) => {
+        setApiCallsLeft(callsRemaining);
+
+        return response.json();
+      })
+      .then((response) => {
         if (response.data.length === 0) {
-            return;
-          }
+          return;
+        }
 
         setNearbyCitiesArr(response.data);
-    })
-    .catch((err) => {console.log(err)});
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const handleClick = (e, city) => {
+    e.preventDefault();
+
+    if (activeButton !== city.id) {
+      setActiveButton(city.id);
+      setSelectedCity(city);
+    } else {
+      setActiveButton(null);
+      setSelectedCity(null);
+    }
   };
 
   useEffect(() => {
-
-    setTimeout(() => {getNearbyCities(mainCity.id)}, 500);
+    setTimeout(() => {
+      getNearbyCities(mainCity.id);
+    }, 500);
 
     return function () {
-        setNearbyCitiesArr([]);
-    }
-  }, [mainCity])
+      setNearbyCitiesArr([]);
+    };
+  }, [mainCity]);
 
   return (
     <div className={"nearby-cities"}>
-
-          {nearbyCitiesArr.map(city => (
-
-                  <button key={city.id}>
-                      {city.name}
-                      {city.countryCode !== mainCity.countryCode
-                        ? ` (${city.countryCode})`
-                        : ''}
-                        {console.log(city)}
-                      </button>
-
-          ))}
-
+      <div className={"nearby-cities__buttons"}>
+        {nearbyCitiesArr.map((city) => (
+          <button
+            key={city.id}
+            onClick={(e) => handleClick(e, city)}
+            className={`nearby-cities__button ${
+              city.id === activeButton ? "active" : ""
+            }`}
+          >
+            {/* using closure with 'city' in handleClick */}
+            {city.name}
+            {city.countryCode !== mainCity.countryCode
+              ? ` (${city.countryCode})`
+              : ""}
+          </button>
+        ))}
+      </div>
+      <div className="nearby-cities__info">
+        {!selectedCity ? (
+          ""
+        ) : (
+          <table className="nearby-cities__table">
+            <tr>
+              <td>Name</td>
+              <td>{selectedCity.name}</td>
+            </tr>
+            <tr>
+              <td>Region</td>
+              <td>{selectedCity.region}</td>
+            </tr>
+            <tr>
+              <td>Population</td>
+              <td>{selectedCity.population}</td>
+            </tr>
+            <tr>
+              <td>Distance (km)</td>
+              <td>{selectedCity.distance * 1.608}</td>
+            </tr>
+          </table>
+        )}
+      </div>
     </div>
   );
 }
