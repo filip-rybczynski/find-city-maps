58d8f9f3ee3395d15408847c4a05468939120db2
Filip Rybczyński <filipryba@gmail.com>
2022-06-19 15:05:35 +0200

    JS: Fixed capitalizing function to capitalize all words in string

--- src/functions/capitalize.js
+++ src/functions/capitalize.js
@@ -1,3 +1,9 @@
-export default function capitalize (string) {
-    return string.charAt(0).toUpperCase() + string.toLowerCase().slice(1);
+export default function capitalize(string) {
+    // Split into separate words
+  const words = string.split(" ");
+
+  return words.map(
+    // Capitalize all words
+    (word) => word.charAt(0).toUpperCase() + word.toLowerCase().slice(1)
+  ).join(" "); // Join again
 }
