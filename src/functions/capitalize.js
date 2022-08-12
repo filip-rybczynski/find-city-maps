e31ff955d3fa31489d41a7e08805a7300f0f935b
Filip Rybczyński <filipryba@gmail.com>
2022-08-12 23:28:17 +0200

    Made capitalize function to account for hyphens

--- src/functions/capitalize.js
+++ src/functions/capitalize.js
@@ -1,9 +1,14 @@
+//! function can also handle hyphenated names, but does not account for exceptions where some words might not need capitalization (e.g. for name with prepositions in them such as Frankfurt am Main)
+
 export default function capitalize(string) {
-    // Split into separate words
-  const words = string.split(" ");
+  
+    // Split into separate words (space and hyphens as delimiters)
+  const words = string.split(/([\s\-])/g); // includes delimiters in the strings
 
   return words.map(
-    // Capitalize all words
-    (word) => word.charAt(0).toUpperCase() + word.toLowerCase().slice(1)
-  ).join(" "); // Join again
+    // Capitalize all words - except special cases
+    (word) => {
+      return word.charAt(0).toUpperCase() + word.toLowerCase().slice(1)
+    }
+  ).join(""); // Join again
 }
